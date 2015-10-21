03347dbb74aaf6c56686c7b501f51f4b6242b90a
Michael Wood <michael.wood@optimizely.com>
2015-10-21 15:19:29 -0700

    fix here syntax for bot

--- files/default/slack_handler_webhook.rb
+++ files/default/slack_handler_webhook.rb
@@ -42,7 +42,7 @@ class Chef::Handler::Slack < Chef::Handler
           if run_status.success?
             slack_message(" :white_check_mark: Chef client run #{run_status_human_readable} on #{run_status.node.name} #{run_status_detail(webhook['detail_level'])}", webhook['url']) unless webhook['fail_only']
           else
-            slack_message(" :skull: @here Chef client run #{run_status_human_readable} on #{run_status.node.name} #{run_status_detail(webhook['detail_level'])}", webhook['url'])
+            slack_message(" :skull: <!channel> Chef client run #{run_status_human_readable} on #{run_status.node.name} #{run_status_detail(webhook['detail_level'])}", webhook['url'])
           end
         end
       end
